e338b273d4d7c52b806daeb602ade5d82935a613
iabhishek12 <abhishekbohra1208@gmail.com>
2023-10-21 02:04:32 +0530

    qr code generator

--- Backend/2.3 NPM/index.js
+++ Backend/2.3 NPM/index.js
@@ -1,5 +1,10 @@
-var generateName = require('sillyname');
-var sillyName = generateName(); // method generateName
+// var generateName = require("sillyname"); 
+import generateName from "sillyname"; 
+var sillyname = generateName() ; 
+console.log(`my name is ${sillyname}`); 
 
+// const superheroes = require('superheroes'); 
+import superheroes from "superheroes";
+var name = superheroes.random(); 
+console.log(`i am ${name}`); 
 
-console.log(`My name is ${sillyName}`); 
